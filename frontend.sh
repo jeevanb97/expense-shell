340053a8dfc57b18d852c780720fc540af614a3c
jeevanb97 <1097sai@gmail.com>
2023-11-21 07:18:23 +0530

    shell sample

---
+++ frontend.sh
@@ -0,0 +1,16 @@
+#first for UI we are installing Nginx
+dnf install nginx -y
+#next we are removing the default UI content that comes with nginx
+rm -rf /usr/share/nginx/html/*
+#bring content from net for UI
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+#unzip it to /usr/share/nginx/html/
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+
+#copy reverse proxy configuration
+cp /expense.conf  /etc/nginx/default.d/expense.conf
+
+#start nginx
+systemctl enable nginx
+systemctl restart nginx
